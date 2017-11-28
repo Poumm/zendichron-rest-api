5998d083003dd6765d40db8b83e686441690eeb8
jl <jl@axin.fr>
2017-11-28 14:03:29 +0100

    allow cross origin

--- app.js
+++ app.js
@@ -9,8 +9,32 @@ mongoose.Promise = global.Promise;
 if (process.env.NODE_ENV !== "test")
   mongoose.connect("mongodb://localhost/zendichron", { useMongoClient: true });
 
-// Doivent être définies dans cet ordre
-// En effet l'ordre défini ici est l'ordre dans lequel les middleware sont appelés
+// Add headers
+app.use(function(req, res, next) {
+  // allow access to react Client
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+//the order define the order of execution
 app.use(bodyParser.json());
 
 app.get("/", function(req, res) {

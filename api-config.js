73237d2b1e9e86c86084829445f05a6b46b2479c
jl <jl@axin.fr>
2018-02-08 13:50:28 +0100

    heroku cross origin

--- api-config.js
+++ api-config.js
@@ -4,7 +4,7 @@ let allowOrigin;
 if (process.env === null) {
   dbUrl =
     "mongodb://heroku_vt6fzffx:j53rlop3u824imtkglit49o440@ds125588.mlab.com:25588/heroku_vt6fzffx";
-  allowOrigin = "https://zendichron.herokuapp.com/";
+  allowOrigin = "https://zendichron.herokuapp.com";
 } else if ((process.env.NODE_ENV = "test")) {
   dbUrl = "mongodb://localhost/zendichron_test";
   allowOrigin = "http://localhost:3000";
